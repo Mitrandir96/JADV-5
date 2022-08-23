efac17e1f38686c19cb86aae504802c0eb15c41c
Илья Грачев <grachev.ia96@gmail.com>
2022-08-23 18:38:26 +0700

    First commit

---
+++ src/main/java/ru/netology/CalcCredit.java
@@ -0,0 +1,16 @@
+package ru.netology;
+
+public class CalcCredit {
+
+    public static double monthlyPayment (int amount, int month, double rate) {
+        return 0;
+    }
+
+    public static double amountReturn (int amount, int month, double rate) {
+        return 0;
+    }
+
+    public static double overPayment (int amount, int month, double rate) {
+        return 0;
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.*;
+import ru.netology.CalcCredit;
+
+import java.util.Random;
+
+public class Test {
+
+    @BeforeAll
+    public static void initSuite() {
+        System.out.println("Tests started");
+    }
+
+    @BeforeEach
+    public void initTest() {
+        System.out.println("Starting new Test");
+    }
+
+    @AfterEach
+    public void finalizeTest() {
+        System.out.println("Test complete");
+    }
+
+    @AfterAll
+    public static void completeSuit() {
+        System.out.println("Tests complete");
+    }
+
+    Random random = new Random();
+
+    @org.junit.jupiter.api.Test
+    public void testMonthlyPayment() {
+        CalcCredit calcCredit = new CalcCredit();
+        int amount = random.nextInt();
+        int month = random.nextInt();
+        double rate = random.nextDouble();
+        double annuityRatio = (rate / 100 / 12 * Math.pow((1 + rate / 100 / 12), month)) / (Math.pow((1 + rate / 100 / 12), month) - 1);
+        double monthlyPayment = calcCredit.monthlyPayment(amount, month, rate);
+        Assertions.assertTrue(amount * annuityRatio == monthlyPayment, "Неверный расчет ежемесячного платежа");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void AmountReturn() {
+        CalcCredit calcCredit = new CalcCredit();
+        int amount = random.nextInt();
+        int month = random.nextInt();
+        double rate = random.nextDouble();
+        double amountReturn = calcCredit.amountReturn(amount, month, rate);
+        Assertions.assertTrue(amount * (rate / 100 / 12 * Math.pow((1 + rate / 100 / 12), month)) /
+                (Math.pow((1 + rate / 100 / 12), month) - 1) * month == amountReturn, "Неверный расчет суммы к возврату");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void OverPayment() {
+        CalcCredit calcCredit = new CalcCredit();
+        int amount = random.nextInt();
+        int month = random.nextInt();
+        double rate = random.nextDouble();
+        double overPayment = calcCredit.overPayment(amount, month, rate);
+        Assertions.assertTrue(amount * (rate / 100 / 12 * Math.pow((1 + rate / 100 / 12), month)) /
+                (Math.pow((1 + rate / 100 / 12), month) - 1) * month - amount == overPayment, "Неверный расчет суммы переплаты");
+
+
+    }
+
+}
